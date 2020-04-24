56c39bd5279adf86bc12d213bd378373addc7dc5
Samya Thakur <thakursamya@gmail.com>
2020-04-24 22:25:14 +0530

    Updates

---
+++ precache-manifest.f4622b8110d2c11d35ca67e7c27ff82d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc8485b5c54035e37508147d391fdf9",
+    "url": "/monster-class/index.html"
+  },
+  {
+    "revision": "9d360c7153088430856f",
+    "url": "/monster-class/static/css/2.2dc38268.chunk.css"
+  },
+  {
+    "revision": "0a0fa63eb83a837ebbd8",
+    "url": "/monster-class/static/css/main.1a78bb34.chunk.css"
+  },
+  {
+    "revision": "9d360c7153088430856f",
+    "url": "/monster-class/static/js/2.0d0ca025.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-class/static/js/2.0d0ca025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0fa63eb83a837ebbd8",
+    "url": "/monster-class/static/js/main.55058a44.chunk.js"
+  },
+  {
+    "revision": "1b6cc6c4d31efad6c747",
+    "url": "/monster-class/static/js/runtime-main.bfd2f4a4.js"
+  }
+]);
